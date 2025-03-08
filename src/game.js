0a70a1f23399f8e9ba716969fc9e1310680af536
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-03-08 10:11:36 +0100

    --

--- src/game.js
+++ src/game.js
@@ -54,4 +54,156 @@ class Game {
       this.gameLoop();
     }, this.gameLoopFrequency);
 
-   
+    //makes a wildfire appear every 10 seconds
+    this.wildfireIntervalId = setInterval(() => {
+      this.wildfire = new Wildfire(
+        this.gameScreen,
+        Math.floor(Math.random() * 800),
+        Math.floor(Math.random() * (550 - 160 + 1) + 160),
+        50,
+        50,
+        "images/wildfire.png"
+      );
+
+      this.wildfire.removeBonus();
+    }, 10000);
+  }
+
+  gameLoop() {
+    //console.log("in the gameLoop")
+    this.update();
+
+    if (this.gameIsOver) {
+      clearInterval(this.wildfireIntervalId);
+      clearInterval(this.gameIntervalId);
+    }
+  }
+
+  update() {
+    this.character.move();
+
+    if (this.wildfire && this.character.didCollide(this.wildfire)) {
+      if (this.wildfire.element) {
+        this.wildfire.element.remove();
+        this.catched++;
+        this.catchedSpan.innerHTML = `${this.catched} / 5`;
+      }
+    }
+
+    if (this.catched === 1) {
+      this.gameIsWon = true;
+      this.endGame();
+    }
+
+    for (let i = 0; i < this.missiles.length; i++) {
+      const missile = this.missiles[i];
+
+      if (missile.element.src.includes("lechuga")) {
+        missile.rotate = "rotate";
+        missile.toggleRotation();
+      }
+
+      missile.move();
+
+      if (this.character.didCollide(missile)) {
+        missile.element.remove();
+        this.lives--;
+        this.livesSpan.innerHTML = this.lives;
+        this.missiles.splice(i, 1);
+        i--;
+      } else if (missile.top >= this.height) {
+        missile.element.remove();
+        this.score += 5;
+        this.scoreSpan.innerHTML = this.score;
+        this.missiles.splice(i, 1);
+        i--;
+      }
+
+      if (this.lives === 0) {
+        this.endGame();
+      }
+    }
+
+    const missileOptions = [
+      {
+        weight: 1,
+        imgPath: "images/lechuga.png",
+      },
+      {
+        weight: 2,
+        imgPath: "images/tomatito.png",
+      },
+      {
+        weight: 3,
+        imgPath: "images/poop.png",
+      },
+    ];
+
+    const randomOption = missileOptions[Math.floor(Math.random() * 3)];
+
+    if (Math.random() > 0.98 && this.missiles.length < 10) {
+      this.missiles.push(
+        new Missile(
+          this.gameScreen,
+          Math.floor(Math.random() * 900),
+          0,
+          100,
+          50,
+          randomOption.weight,
+          randomOption.imgPath
+        )
+      );
+    }
+  }
+
+  changeStatsDisplay() {
+    this.statsContainer.removeAttribute("id", "stats-container");
+    this.statsContainer.setAttribute("id", "stats-in-game");
+  }
+
+  endGame() {
+    this.character.element.remove();
+    this.missiles.forEach((missile) => missile.element.remove());
+    clearInterval(this.wildfireIntervalId);
+    if (this.wildfire) {
+      this.wildfire.element.remove();
+    }
+
+    this.gameIsOver = true;
+
+    const endScreenResult = document.getElementById("game-result");
+    const endPhrase = document.getElementById("end-phrase");
+    const restartButton = document.getElementById("restart-button")
+
+    this.gameScreen.style.display = "none";
+    this.gameEndScreen.style.display = "flex";
+    this.gameEndScreen.style.width = `${this.width}px`;
+    this.gameEndScreen.style.height = `${this.height}px`;
+
+    if(this.gameIsWon){
+        endScreenResult.innerHTML = "You won!";
+        endPhrase.innerHTML = "Press space for your revenge";
+        restartButton.innerHTML = "Play again"
+        window.addEventListener("keydown", (event) => {
+            if(event.key === " ") {
+                this.gameEndScreen.style.backgroundImage = "url(../images/septExpld.gif)";
+                this.gameEndScreen.style.backgroundSize = "cover";
+                endScreenResult.innerHTML = "";
+                endPhrase.innerHTML = "";
+            }
+        })
+    }
+
+    this.scoreSpan.innerHTML = this.score;
+    this.catchedSpan.innerHTML = `${this.catched} / 5`;
+
+    this.music.pause();
+    this.music.currentTime = 0;
+  }
+
+  playMusic() {
+    this.music.addEventListener("ended", function () {
+      return true;
+    });
+  }
+}
--- src/script.js
+++ src/script.js
@@ -62,4 +62,21 @@ window.onload = function () {
 
       switch (key) {
         case "ArrowLeft":
-          game.character.di
+          game.character.directionX = 0;
+          break;
+        case "ArrowUp":
+          game.character.directionY = 0;
+          break;
+        case "ArrowRight":
+          game.character.directionX = 0;
+          break;
+        case "ArrowDown":
+          game.character.directionY = 0;
+          break;
+      }
+    }
+  }
+
+  window.addEventListener("keydown", handleKeyDown);
+  window.addEventListener("keyup", handleKeyUp);
+};
